3a3c7bf990c4c6004a66e03f77363039544f3935
rivo <rivo>
2021-03-10 17:37:50 +0300

    test wasm

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+use eframe::{
+    egui,
+    epi,
+};
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+pub struct TemplateApp {
+    repl: String,
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        Self {
+            repl: String::new(),
+        }
+    }
+}
+
+impl epi::App for TemplateApp {
+    fn name(&self) -> &str {
+        "test"
+    }
+
+    /// Called by the framework to load old app state (if any).
+    #[cfg(feature = "persistence")]
+    fn load(&mut self, storage: &dyn epi::Storage) {
+        *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+    }
+
+    /// Called by the frame work to save state before shutdown.
+    #[cfg(feature = "persistence")]
+    fn save(&mut self, storage: &mut dyn epi::Storage) {
+        epi::set_value(storage, epi::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        let TemplateApp { repl } = self;
+
+        egui::TopPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        frame.quit();
+                    }
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Test");
+            ui.separator();
+            ui.heading("Central Panel");
+            ui.text_edit_singleline(repl);
+        });
+    }
+}
