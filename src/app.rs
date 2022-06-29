bda852ecdad747b9c63bc9bbfcf0c6693e3f05a9
rivo <rivo>
2022-06-29 12:56:37 +0300

    tune menu again

--- src/app.rs
+++ src/app.rs
@@ -493,7 +493,8 @@ impl TemplateApp {
 
         let mut rnext_enabled = false;
         let rollback_enabled = self.snapshot.is_some() && !self.is_trial();
-        egui::SidePanel::right("menu").show(ctx, |ui| {
+        egui::TopBottomPanel::top("menu").show(ctx, |ui| {
+            ui.horizontal_wrapped(|ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button(self.menu_text("Open...")).clicked() {
                         open_clicked = true;
@@ -517,35 +518,35 @@ impl TemplateApp {
                         self.theme_editor = !self.theme_editor;
                         ui.close_menu();
                     }
+
                 });
-            run_clicked = ui.button(self.menu_text("🚀 Run")).clicked();
+            run_clicked = ui.button(self.menu_text("🚀Run")).clicked();
                 snapshot_clicked = ui
-                    .add_enabled(!self.is_trial(), Button::new(self.menu_text("💾 Snapshot"))
+                    .add_enabled(!self.is_trial(), Button::new(self.menu_text("💾Snapshot"))
                     .wrap(false))
                     .clicked();
                 rollback_clicked = ui
-                    .add_enabled(rollback_enabled, Button::new(self.menu_text("🔨 Rollback"))
+                    .add_enabled(rollback_enabled, Button::new(self.menu_text("🔨Rollback"))
                     .wrap(false))
                     .clicked();
                 let unfreeze_enabled = self.frozen_code.iter().any(|c| match c { FrozenStr::Code(_) => true, _ => false });
-                unfreeze_clicked = ui.add_enabled(unfreeze_enabled, Button::new(self.menu_text("🔥 Unfreeze"))
+                unfreeze_clicked = ui.add_enabled(unfreeze_enabled, Button::new(self.menu_text("🔥Unfreeze"))
                 .wrap(false))
                 .clicked();
-                repl_clicked = ui
-                    .radio(self.trial_code.is_none(), self.menu_text("REPL"))
-                    .clicked();
-                trial_clicked = ui
-                    .radio(self.trial_code.is_some(), self.menu_text("TRIAL"))
-                    .clicked();
-                ui.checkbox(&mut self.rdebug_enabled, "Reverse");
+                let mut trial_mode = self.trial_code.is_some();
+                if ui.checkbox(&mut trial_mode, "TRIAL").changed() {
+                    repl_clicked = !trial_mode;
+                    trial_clicked = trial_mode;
+                }
+                ui.checkbox(&mut self.rdebug_enabled, "RDebug");
                 self.xs.set_recording_enabled(self.rdebug_enabled);
                 if self.xs.is_recording() {
                     rnext_enabled = self.rlog_size().map(|n| n > 0).unwrap_or(false);
                     rnext_clicked = ui
-                        .add_enabled(rnext_enabled, Button::new(self.menu_text("↩ RNext")))
+                        .add_enabled(rnext_enabled, Button::new(self.menu_text("↩RNext")))
                         .clicked();
                     next_clicked = ui
-                        .add_enabled(self.xs.is_running(), Button::new(self.menu_text("↪ Next")))
+                        .add_enabled(self.xs.is_running(), Button::new(self.menu_text("↪Next")))
                         .clicked();
                 }
                 ui.menu_button("Help", |ui| {
@@ -564,11 +565,12 @@ impl TemplateApp {
                         self.help.mode = HelpMode::QuickRef;
                         ui.close_menu();
                     }
-                    ui.menu_button("Examples", |ui| {
-                        self.menu_examples(ui);
-                    });
                     ui.hyperlink_to("Youtube", "https://www.youtube.com/channel/UCYTeJIi6aLE9rS7s_QOto3w");
+                    ui.add_enabled(false, Label::new("Examples:"));
+                    self.menu_examples(ui);
+
                 });
+            });
         }); // top panel
 
         egui::SidePanel::left("left_panel").show(ctx, |ui| {
